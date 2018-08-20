172afaf6d78e921dfb4d4968e22a397e85c43e1d
KCNT <korn.chinnatan@gmail.com>
2018-08-20 14:00:28 +0700

    New Fragment Class and Add id to fragment xml

---
+++ app/src/main/java/com/healthy/a59070040/healthy/RegisterFragment.java
@@ -0,0 +1,18 @@
+package com.healthy.a59070040.healthy;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RegisterFragment extends Fragment {
+
+    @NonNull
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState) {
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+}
