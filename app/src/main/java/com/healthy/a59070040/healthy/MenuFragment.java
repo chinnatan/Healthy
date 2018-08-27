1621c6dc0d1e92a7b8197d6a805838342aa806dd
KCNT <korn.chinnatan@gmail.com>
2018-08-27 20:51:11 +0700

    Delete Weight System for find bug in app

--- app/src/main/java/com/healthy/a59070040/healthy/MenuFragment.java
+++ app/src/main/java/com/healthy/a59070040/healthy/MenuFragment.java
@@ -12,8 +12,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.healthy.a59070040.healthy.weight.WeightFragment;
-
 import java.util.ArrayList;
 
 public class MenuFragment extends Fragment {
@@ -42,7 +40,7 @@ public class MenuFragment extends Fragment {
                     getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_view, new BMIFragment()).addToBackStack(null).commit();
                     Log.d("MENU", "Selected on BMI Menu");
                 } else if(_menu.get(i).equals("Weight")){
-                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_view, new WeightFragment()).addToBackStack(null).commit();
+                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_view, new BMIFragment()).addToBackStack(null).commit();
                     Log.d("MENU", "Selected on Weight Menu");
                 }
                 _menuAdapter.notifyDataSetChanged();
