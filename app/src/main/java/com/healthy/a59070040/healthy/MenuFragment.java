d7e96cae590ff7c69c03f8cb9dcb4d850390a666
KCNT <korn.chinnatan@gmail.com>
2018-08-29 09:10:46 +0700

    Menu constructor completed

--- app/src/main/java/com/healthy/a59070040/healthy/MenuFragment.java
+++ app/src/main/java/com/healthy/a59070040/healthy/MenuFragment.java
@@ -19,46 +19,43 @@ import java.util.ArrayList;
 
 public class MenuFragment extends Fragment {
 
-    ArrayList<String> _menu = new ArrayList<>();
-
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        Menu _menus = new Menu();
-        _menus.addItem("BMI");
-        _menus.addItem("Weight");
-        _menus.addItem("Setup");
-
-        _menu.clear();
-        _menu.add("BMI");
-        _menu.add("Weight");
-        _menu.add("Setup");
-
-        ListView _menuList = getView().findViewById(R.id.menu_list);
-        final ArrayAdapter<String> _menuAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, _menu);
-
-        _menuList.setAdapter(_menuAdapter);
-
-        _menuList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Log.d("MENU", "Select on " + _menu.get(i));
-                if(_menu.get(i).equals("BMI")) {
-                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_view, new BMIFragment()).addToBackStack(null).commit();
-                    Log.d("MENU", "Selected on BMI Menu");
-                } else if(_menu.get(i).equals("Weight")){
-                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_view, new WeightFragment()).addToBackStack(null).commit();
-                    Log.d("MENU", "Selected on Weight Menu");
-                }
-                _menuAdapter.notifyDataSetChanged();
-            }
-        });
+        initMenuList();
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_menu, container, false);
+    }
+
+    void initMenuList() {
+        final Menu _menus = new Menu();
+        _menus.addItem("BMI");
+        _menus.addItem("Weight");
+        _menus.addItem("Setup");
+
+        ListView _menuList = getView().findViewById(R.id.menu_list);
+        final ArrayAdapter<String> _menuAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, _menus.getMenu());
+
+        _menuList.setAdapter(_menuAdapter);
+
+        _menuList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("MENU", "Select on " + _menus.getMenu().get(i));
+                if(_menus.getMenu().get(i).equals("BMI")) {
+                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_view, new BMIFragment()).addToBackStack(null).commit();
+                    Log.d("MENU", "Selected on BMI Menu");
+                } else if(_menus.getMenu().get(i).equals("Weight")){
+                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_view, new WeightFragment()).addToBackStack(null).commit();
+                    Log.d("MENU", "Selected on Weight Menu");
+                }
+                _menuAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }
